cd2c803029f8a0cd937439ff310603d4d561e1b9
nikunj ladva <nikunjladva245@gmail.com>
2021-08-13 15:40:22 +0530

    working on a chat component by using chat engine

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React , {useRef , useState , useEffect} from 'react';
+import { useHistory } from 'react-router-dom';
+import {ChatEngine} from 'react-chat-engine';
+import { auth } from '../firebase';
+import {useAuth} from '../contexts/AuthContext'
+import axios from 'axios';
+//put all private key into envirment variable
 
 const Chats = () => {
-    return (
-        <div>
-            <h1> chat componet </h1>
+    const history = useHistory();
+    const {user} = useAuth();
+    const [loading, setloading] = useState(true);
+
+    const handleLogout = async ()=>{
+        await auth.signOut();
+        history.push('/');
+    }
+ 
+    const getFile = async (url)=>{
+        const response = await fetch(url);
+        const data = await response.blob();
+
+        return new File([data] , "userPhoto.jpeg" , {type : 'image/jpeg'})
+    }
+    
+    useEffect(()=>{
+         if (!user) {
+             history.push('/');
+             return;
+         }
+         axios.get('https://api.chatengine.io/users/me',{
+             headers : {
+                "project-id":process.env.REACT_CHAT_ENGINE_ID,
+                "user-name" : user.email ,
+                "user-secret":user.uid
+             }
+         })
+         .then(()=>{
+            setloading(false);    
+         })
+         .catch(()=>{
+             let formdata = new FormData();
+             formdata.append('email',user.email);
+             formdata.append('username',user.email);
+             formdata.append('secret',user.uid);
+            
+              getFile(user.photoURL)
+              .then((avatar)=>{
+                 formdata.append('avatar' , avatar , avatar.name);
+
+                 axios.post('https://api.chatengine.io/users',formdata,{
+                     headers :{ "private-key" : process.env.REACT_CHAT_ENGINE_KEY}
+                 })
+                 .then(()=>{setloading(false)})
+                 .catch((error)=> console.log(error))
+              })
+         })
+     }, [user , history]);
+
+     if(!user || loading) return 'loading...';    
+     
+     return (
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Webchat
+                </div>
+                <div onClick={handleLogout} className="logout-tab">
+                    Logout
+                </div>
+            </div>
+                
+                <ChatEngine 
+                   hight="calc(100vh-66px)"
+                   projectID={process.env.REACT_CHAT_ENGINE_ID}
+                   userName={user.email}
+                   userSecret={user.uid}  
+                />
         </div>
     )
 }
 
-export default Chats
+export default Chats;
